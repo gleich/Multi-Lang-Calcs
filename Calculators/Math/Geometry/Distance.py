1ad40cb239d7bf78cbabfd23e954de90c8e0363e
Matthew Gleich <matthewgleich@gmail.com>
2019-01-27 17:43:57 -0500

    Create Distance.py

---
+++ Calculators/Math/Geometry/Distance.py
@@ -0,0 +1,17 @@
+#Instructions:
+#Go the bottom of the file and edit the inputs for what you need done
+#--------------------------------------------------------------------
+def distance(line_points):
+    x_movements = (line_points[1] - line_points[0]) ** 2
+    y_movements = (line_points[3] - line_points[2]) ** 2
+    added_movements = x_movements + y_movements
+    final_square_root = added_movements**(0.5)
+    print("")
+    print("------------------------")
+    print("The distance formula is:")
+    print(final_square_root)
+    print("------------------------")
+    print("")
+
+#Example:
+distance([10,2,7,5])
