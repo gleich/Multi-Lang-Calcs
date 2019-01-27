4bf38359b88713f31b600d6c90da5da2f167b915
Matthew Gleich <matthewgleich@gmail.com>
2019-01-27 17:44:00 -0500

    Create Midpoint.py

---
+++ Calculators/Math/Geometry/Midpoint.py
@@ -0,0 +1,10 @@
+#Instructions:
+#Go the bottom of the file and edit the inputs for what you need done
+#--------------------------------------------------------------------
+def midpoint(line_points):
+    point_one = (line_points[0] + line_points[3]) / 2
+    point_two = (line_points[1] + line_points[2]) / 2
+    print(point_one,point_two)
+
+#Example:
+midpoint([3,4,8,1])
