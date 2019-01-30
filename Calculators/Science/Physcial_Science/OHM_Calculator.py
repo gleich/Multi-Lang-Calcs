0ee95b35b9540f2e1a7d0f29e0f4617eb7b0f223
Matthew Gleich <matthewgleich@gmail.com>
2019-01-30 14:32:48 -0500

    Create OHM_Calculator.py

---
+++ Calculators/Science/Physcial_Science/OHM_Calculator.py
@@ -0,0 +1,48 @@
+
+while random_continious_number = 0:
+    exchange = input("What are you trying to find (Voltage, Current(Ampage), or Resistance)\n")
+    if "vol" in exchange.lower():
+        print("")
+        amps = input("What is the amount of amps?\n")
+        print("")
+        resistance = input("What is the amount of resistance?\n")
+        print("")
+        amps_int = int(amps)
+        resistance_int = int(resistance)
+        voltage = amps_int * resistance_int
+        print("The voltage is:",voltage,"volts\n")
+        continue_question = input("Would you like to do another calculation?")
+        if continue_question == "y" in continue_question.lower():
+            continue
+        else:
+            break
+    elif "cur" in exchange.lower():
+        print("")
+        volts = input("What is the amount of volts?\n")
+        print("")
+        resistance = input("What is the amount of resistance?\n")
+        print("")
+        volts_int = int(volts)
+        resistance_int = int(resistance)
+        current = volts_int / resistance_int
+        print("The current(Ampage) is:",current+"amps\n")
+        continue_question = input("Would you like to do another calculation?")
+        if continue_question == "y" in continue_question.lower():
+            continue
+        else:
+            break
+    elif "res" in exchange.lower():
+        print("")
+        volts = input("What is the amount of volts?\n")
+        prin("")
+        amps = input("What is the amount of amps?\n")
+        print("")
+        volts_int = int(volts)
+        amps_int = int(amps)
+        resistance = volts_int / amps_int
+        print("The resistance is:",resistance,"(Ω)")
+        continue_question = input("Would you like to do another calculation?")
+        if continue_question == "y" in continue_question.lower():
+            continue
+        else:
+            break
