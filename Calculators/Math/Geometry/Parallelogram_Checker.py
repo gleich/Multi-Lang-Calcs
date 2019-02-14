3b06b3fca3fc58edabf2f3256c9534dedde90325
Matthew Gleich <matthewgleich@gmail.com>
2019-02-13 23:48:56 -0500

    Finally Fixed and now works with Squares, rectangles, rhombuses, trapezoids, and isosceles trapezoids. Degrees still don't work

--- Calculators/Math/Geometry/Parallelogram_Checker.py
+++ Calculators/Math/Geometry/Parallelogram_Checker.py
@@ -1,73 +1,148 @@
-# This file is still being rebuilt and can't be used right now. It will be done in a few days or so...
-#--------------------------------------------------------------------
+    # This file is still being rebuilt and can't be used right now. It will be done in a few days or so...
+    #--------------------------------------------------------------------
+while 0 == 0:
+    # Distance formula:
+    def distance_finder(x1,y1,x2,y2):
+        distance = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+        return distance
 
-# Distance formula:
-def distance_finder(x1,y1,x2,y2):
-    distance = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
-    return distance
+    def slope_finder(x1,y1,x2,y2):
+        slope = ((y2 - y1) / (x2 - x1))
+        return slope
 
-inital_input = input("What are you given, points or degrees?\n")
+    initial_input = input("What are you given, points or degrees?\n")
 
-if "p" in inital_input.lower():
-    first_point_input_question = \
-    """
-    For this step you will need to graph the shape.
-    Once you have graphed them please type the coordinates of each point counter-clockwise
-    Please seperate each point by a comma!
-    """
-    points = input(first_point_input_question)
-    points_list_org = points.split(",")
-    points_list = []
+    if "p" in initial_input.lower():
+        print("")
+        points = input("Please list all the coordinates of each point counter-clockwise. Please separate each point by a comma.\n")
+        points_list_org = points.split(",")
+        points_list = []
 
-    for number in points_list_org:
-        new_number = int(number)
-        points_list.append(new_number)
+        for number in points_list_org:
+            new_number = int(number)
+            points_list.append(new_number)
 
-    #Point 1 Coordinates:
-    point1_x = points_list[0]
-    point1_y = points_list[1]
+        #Point 1 Coordinates:
+        point1_x = points_list[0]
+        point1_y = points_list[1]
 
-    #Point 2 Coordinates:
-    point2_x = points_list[2]
-    point2_y = points_list[3]
+        #Point 2 Coordinates:
+        point2_x = points_list[2]
+        point2_y = points_list[3]
 
-    #Point 3 Coordinates:
-    point3_x = points_list[4]
-    point3_y = points_list[5]
+        #Point 3 Coordinates:
+        point3_x = points_list[4]
+        point3_y = points_list[5]
 
-    #Point 4 Coordinates:
-    point4_x = points_list[6]
-    point4_y = points_list[7]
+        #Point 4 Coordinates:
+        point4_x = points_list[6]
+        point4_y = points_list[7]
 
-    distance_from_1to2S = distance_finder(point1_x,point1_y,point2_x,point2_y)
-    distance_from_3to4S = distance_finder(point3_x,point3_y,point4_x,point4_y)
-    distance_from_2to3S = distance_finder(point2_x,point2_y,point3_x,point3_y)
-    distance_from_4to1S = distance_finder(point4_x,point4_y,point1_x,point1_y)
-    distance_from_
+        distance_from_1to2S = distance_finder(point1_x, point1_y, point2_x, point2_y)
+        distance_from_3to4S = distance_finder(point3_x, point3_y, point4_x, point4_y)
+        distance_from_2to3S = distance_finder(point2_x, point2_y, point3_x, point3_y)
+        distance_from_4to1S = distance_finder(point4_x, point4_y, point1_x, point1_y)
+        distance_from_2to4D = distance_finder(point2_x, point2_y, point4_x, point4_y)
+        distance_from_3to1D = distance_finder(point3_x, point3_y, point1_x, point1_y)
+        slope_of_2to3S = slope_finder(point2_x, point2_y, point3_x, point3_y)
+        slope_of_1to4S = slope_finder(point1_x, point1_y, point4_x, point4_y)
+        slope_of_2to1S = slope_finder(point2_x, point2_y, point1_x, point1_y)
+        slope_of_3to4S = slope_finder(point3_x, point3_y, point4_x, point4_y)
 
-    if distance_from_1to2S == distance_from_3to4S or distance_from_4to1S == distance_from_2to3S:
-        if distance_from_1to2S == distance_from_3to4S and distance_from_4to1S == distance_from_2to3S:
+        if distance_from_1to2S == distance_from_2to3S and distance_from_1to2S == distance_from_4to1S and distance_from_2to4D == distance_from_3to1D:
             print("")
-            print("---------------------")
-            print("The shape is a square\n")
+            print("-------------------------------------------------------")
+            print("The four points form a square\n")
             print("Explanation:")
-            print("The reason that these shape is a square is because there are four congruent sides\n")
+            print("All four sides are the same length and diagonals are the same length\n")
             print("Proof:")
-            print("First distance:",distance_from_1to2S)
-            print("Second distrance:",distance_from_3to4S)
-            print("Third distance:",distance_from_4to1S)
-            print("Fourth distance:",distance_from_2to3S)
-            print("---------------------")
-        elif distance_from_1to2S == distance_from_3to4S and distance_from_4to1S == distance_from_2to3S or distance_from_1to2S == distance_from_2to3S and distance_from_3to4S == distance_from_4to1S:
+            print("Distance from point 1 to point 2 (" + str(distance_from_1to2S) + ") is equal to the distance of point 3 to point 4 (" + str(distance_from_3to4S) + (")"))
+            print("Distance from point 2 to point 3 (" + str(distance_from_2to3S) + ") is equal to the distance of point 4 to point 1 (" + str(distance_from_4to1S) + (")"))
+            print("Distance from point 2 to point 4 (" + str(distance_from_2to4D) + ") is equal to the distance of point 3 to point 1 (" + str(distance_from_3to1D) + (")\n"))
+            continue_question = input("Would you like to do another calculation?\n")
+            if "y" in continue_question.lower():
+                continue
+            else:
+                break
+        elif distance_from_2to4D == distance_from_3to1D and slope_of_1to4S == slope_of_2to3S:
             print("")
-            print("---------------------")
-            print("The shape is a square\n")
+            print("-------------------------------------------------------")
+            print("The four points form a rectangle\n")
             print("Explanation:")
-            print("The reason that these shape is a rectangle is because there are two pairs of congruent sides\n")
+            print("All diagonals are the same length and both sides have the same length\n")
             print("Proof:")
-            print("First distance:",distance_from_1to2S)
-            print("Second distrance:",distance_from_3to4S)
-            print("Third distance:",distance_from_4to1S)
-            print("Fourth distance:",distance_from_2to3S)
-            print("---------------------")
-        elif
+            print("Distance from point 2 to point 4 (" + str(distance_from_2to4D) + ") is equal to the distance of point 3 to point 1 (" + str(distance_from_3to1D) + (")\n"))
+            continue_question = input("Would you like to do another calculation?\n")
+            if "y" in continue_question.lower():
+                continue
+            else:
+                break
+        elif distance_from_1to2S == distance_from_2to3S and distance_from_1to2S == distance_from_4to1S:
+            print("")
+            print("-------------------------------------------------------")
+            print("The four points form a rhombus\n")
+            print("Explanation:")
+            print("All four sides are the same length\n")
+            print("Proof:")
+            print("Distance from point 1 to point 2 (" + str(distance_from_1to2S) + ") is equal to the distance of point 3 to point 4 (" + str(distance_from_3to4S) + (")"))
+            print("Distance from point 2 to point 3 (" + str(distance_from_2to3S) + ") is equal to the distance of point 4 to point 1 (" + str(distance_from_4to1S) + (")\n"))
+            continue_question = input("Would you like to do another calculation?\n")
+            if "y" in continue_question.lower():
+                continue
+            else:
+                break
+        elif slope_of_2to1S == slope_of_3to4S:
+            print("")
+            print("-------------------------------------------------------")
+            print("The four points form a Trapezoid\n")
+            print("Explanation:")
+            print("All base sides are congruent and all diagonals are not congruent. All legs don't have the same slope as well.\n")
+            print("Proof:")
+            print("Distance from point 1 to point 2 (" + str(distance_from_1to2S) + ") is equal to the distance of point 3 to point 4 (" + str(distance_from_3to4S) + (")"))
+            print("Distance from point 2 to point 3 (" + str(distance_from_2to3S) + ") is equal to the distance of point 4 to point 1 (" + str(distance_from_4to1S) + (")"))
+            print("Distance from point 2 to point 4 (" + str(distance_from_2to4D) + ") is equal to the distance of point 3 to point 1 (" + str(distance_from_3to1D) + (")\n"))
+            print("Other info")
+            print("Length of top: " + str(distance_from_3to4S))
+            length_of_middle = 0.5 * (distance_from_3to4S + distance_from_1to2S)
+            print("Length of middle: " + str(length_of_middle))
+            print("Length of bottom: " + str(distance_from_1to2S))
+            continue_question = input("Would you like to do another calculation?\n")
+            if "y" in continue_question.lower():
+                continue
+            else:
+                break
+        elif distance_from_2to3S == distance_from_4to1S and slope_of_3to4S == slope_of_2to1S and distance_from_2to4D == distance_from_3to1D:
+            print("")
+            print("-------------------------------------------------------")
+            print("The four points form a Isosceles Trapezoid\n")
+            print("Explanation:")
+            print("Both diagonals are congruent, bases are congruent, and legs are congruent\n")
+            print("Proof:")
+            print("Distance from point 1 to point 2 (" + str(distance_from_1to2S) + ") is equal to the distance of point 3 to point 4 (" + str(distance_from_3to4S) + (")"))
+            print("Distance from point 2 to point 3 (" + str(distance_from_2to3S) + ") is equal to the distance of point 4 to point 1 (" + str(distance_from_4to1S) + (")"))
+            print("Distance from point 2 to point 4 (" + str(distance_from_2to4D) + ") is equal to the distance of point 3 to point 1 (" + str(distance_from_3to1D) + (")\n"))
+            print("Other info")
+            print("Length of top: " + str(distance_from_3to4S))
+            length_of_middle = 0.5 * (distance_from_3to4S + distance_from_1to2S)
+            print("Length of middle: " + str(length_of_middle))
+            print("Length of bottom: " + str(distance_from_1to2S))
+            continue_question = input("Would you like to do another calculation?\n")
+            if "y" in continue_question.lower():
+                continue
+            else:
+                break
+        else:
+            print("")
+            print("-------------------------------------------------------")
+            print("The four points don't form a parallelogram\n")
+            print("Explanation:")
+            print("More than two sides are not the same length\n")
+            print("Proof:")
+            print("Distance from point 1 to point 2 (" + str(distance_from_1to2S) + ") is equal to the distance of point 3 to point 4 (" + str(distance_from_3to4S) + (")"))
+            print("Distance from point 2 to point 3 (" + str(distance_from_2to3S) + ") is equal to the distance of point 4 to point 1 (" + str(distance_from_4to1S) + (")"))
+            print("Distance from point 2 to point 4 (" + str(distance_from_2to4D) + ") is equal to the distance of point 3 to point 1 (" + str(distance_from_3to1D) + (")\n"))
+            continue_question = input("Would you like to do another calculation?\n")
+            if "y" in continue_question.lower():
+                continue
+            else:
+                break
