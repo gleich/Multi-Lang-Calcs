da21032914e19dcf5787fb4f6e3f7dc42c08ce38
Matt-Gleich <matthewgleich@gmail.com>
2019-02-20 00:31:25 -0500

    Created Proportional_Ratio_Checker.py

---
+++ Calculators/Math/Geometry/Proportional_Ratio_Checker.py
@@ -0,0 +1,52 @@
+# Instructions:
+# Go the bottom of the file and edit the inputs for what you need done
+# --------------------------------------------------------------------
+while 0 == 0:
+    first_fraction_str = input("What is the first fraction? Please separate the two numbers by a comma!\n")
+    print("")
+    second_fraction_str = input("What is the second fraction? Please separate the two numbers by a comma\n")
+
+    first_fraction_str = first_fraction_str.split(",")
+    second_fraction_str = second_fraction_str.split(",")
+    first_fraction = []
+    second_fraction = []
+
+    for number in first_fraction_str:
+        new_number = int(number)
+        first_fraction.append(new_number)
+
+    for number in second_fraction_str:
+        new_number = int(number)
+        second_fraction.append(new_number)
+
+    fraction1_simplest = (first_fraction[0] / first_fraction[0]) / (first_fraction[1] / first_fraction[0])
+    fraction2_simplest = (second_fraction[0] / second_fraction[0]) / (second_fraction[1] / second_fraction[0])
+
+    if fraction1_simplest == fraction2_simplest:
+        print("")
+        print("The two ratios form a proportion!")
+        print("Ratio one's simplest form:", fraction1_simplest)
+        print("Ratio two's simplest form:", fraction2_simplest)
+        print("")
+        continue_question = input("Would you like to do another calculation?\n")
+        if "y" in continue_question.lower():
+            continue
+        else:
+            break
+    elif fraction1_simplest != fraction2_simplest:
+        print("")
+        print("The two ratios don't a proportion!")
+        print("Ratio one's simplest form:", fraction1_simplest)
+        print("Ratio two's simplest form:", fraction2_simplest)
+        print("")
+        continue_question = input("Would you like to do another calculation?\n")
+        if "y" in continue_question.lower():
+            continue
+        else:
+            break
+    else:
+        print("")
+        print("Please put in a valid data type")
+        for i in range(10):
+            print("")
+        continue
