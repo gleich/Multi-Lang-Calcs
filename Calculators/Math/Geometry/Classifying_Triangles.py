ecac875d2df3ef8dc0bb62a8f05d663525ac6b8d
Matt-Gleich <matthewgleich@gmail.com>
2019-03-21 09:35:20 -0400

    Update Classifying_Triangles.py

--- Calculators/Math/Geometry/Classifying_Triangles.py
+++ Calculators/Math/Geometry/Classifying_Triangles.py
@@ -1,3 +1,26 @@
 #Instructions:
 #Go the bottom of the file and edit the inputs for what you need done
 #--------------------------------------------------------------------
+nums = input("What are the numbers? Seperate each one with a comma\n")
+
+lst_str = nums.split(",")
+lst_int = []
+for string in lst_str:
+    integer = int(string)
+    lst_int.append(integer)
+
+def classify_triangle(lst):
+    sorted_lst = sorted(lst)
+    c = sorted_lst[0] ** 2
+    a = sorted_lst[1] ** 2
+    b = sorted_lst[2] ** 2
+    if c == a + b:
+        print("Triangle is a right triangles because:  " + str(c) + "=" + str(a) + "+" + str(b))
+    elif c > a + b:
+        print("Triangle is a obtuse triangle because:  " + str(c) + ">" + str(a) + "+" + str(b))
+    elif c < a + b:
+        print("Triangle is a acute triangle because:  " + str(c) + "<" + str(a) + "+" + str(b))
+    else:
+        print("Error")
+
+classify_triangle(lst_int)
