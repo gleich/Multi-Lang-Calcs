1bd9a12684067974020e2c39e113834bd1550498
Matt-Gleich <matthewgleich@gmail.com>
2019-03-28 20:51:27 -0400

    Create List_Nums.py

---
+++ Utli/List_Nums.py
@@ -0,0 +1,8 @@
+def question_get_answer(question):
+    """
+    Get the answer to a question using user input
+    :parameter question for the user to answer
+    :return response
+    """
+    answer = input(question,"\n")
+    return answer
