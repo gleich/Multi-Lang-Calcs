4b529eadc73c59979102671a235e3abd7e74cb37
Matt-Gleich <matthewgleich@gmail.com>
2019-04-03 08:33:46 -0400

    Rm from Loop

--- Calculators/Science/Physcial_Science/Motion.py
+++ Calculators/Science/Physcial_Science/Motion.py
@@ -1,28 +1,27 @@
+def list_nums(question, seperator):
+    """
+    Get a list of numbers that the uder supplies.
+    :parameter String which is the question getting asked
+    :parameter What the seperator of the numbers should be
+    :return lst of ints
+    """
+    nums = input("{quest}. Each number should be seperated by a {sep}\n".format(quest=question,sep=seperator))
+    lst_int = []
+    for string in nums.split(seperator):
+        integer = float(string)
+        lst_int.append(integer)
+    return lst_int
+
+def find_motion():
+    """
+    Get the speed of a certain object
+    :return int
+    """
+    numbers = list_nums("What is the time and distance? Example answer: 2.34,400", ",")
+    speed = round(numbers[1] / numbers[0])
+    return speed
+
 while True:
-
-    def list_nums(question, seperator):
-        """
-        Get a list of numbers that the uder supplies.
-        :parameter String which is the question getting asked
-        :parameter What the seperator of the numbers should be
-        :return lst of ints
-        """
-        nums = input("{quest}. Each number should be seperated by a {sep}\n".format(quest=question,sep=seperator))
-        lst_int = []
-        for string in nums.split(seperator):
-            integer = float(string)
-            lst_int.append(integer)
-        return lst_int
-
-    def find_motion():
-        """
-        Get the speed of a certain object
-        :return int
-        """
-        numbers = list_nums("What is the time and distance? Example answer: 2.34,400", ",")
-        speed = round(numbers[1] / numbers[0])
-        return speed
-
     print("Speed:", str(find_motion(), "mph"))
     print("")
     continue_question = input("Would you like to do another calculation?\n")
