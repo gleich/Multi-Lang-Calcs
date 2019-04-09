3d234a6a4823883222fd32a13211c4de18bb6340
Matt-Gleich <matthewgleich@gmail.com>
2019-04-09 18:39:20 -0400

    Added just time funciton

--- Calculators/Science/Physcial_Science/Distance_Finder.py
+++ Calculators/Science/Physcial_Science/Distance_Finder.py
@@ -2,6 +2,7 @@ def find_distance(acceleration_str, time_str):
     acceleration_num = float(acceleration_str)
     time_num = float(time_str)
     distance_answer = (0.5 * acceleration_num * time_num) ** 2
+    distance_rounded = round(distance_answer, 0.01)
     return distance_answer
 
 
@@ -9,7 +10,16 @@ def find_acceleration(distance_str, time_str):
     distance_num = float(distance_str)
     time_num = float(time_str)
     acceleration_answer = (2 * distance_num) / (time_num ** 2)
-    return acceleration_answer
+    acceleration_rounded = round(acceleration_answer, 0.01)
+    return acceleration_rounded
+
+
+def find_distance_time(time_str):
+    time_num = float(time_str)
+    time_answer = 0.5 * time_num ** 2
+    time_rounded = round(time_answer, 0.01)
+    return time_rounded
+
 
 def spacer():
     for i in range(30):
@@ -20,12 +30,17 @@ while True:
     acceleration = input("What is the acceleration? If there is nothing then enter none.\n")
     time = input("What is the time? If there is nothing then enter none\n")
     if "n" in acceleration.lower():
-        distance = input("What is the distance?\n")
+        distance = input("What is the distance? If there is nothing then enter none.\n")
         spacer()
         print("The acceleration is:",find_acceleration(distance, time))
         print("Work:")
         print("(2 * {distance_print_ref}) / ({time_print_ref}^2)".format(distance_print_ref=distance,time_print_ref=time))
         continue_question = input("Would you like to do another calculation?\n")
+        if "n" in distance.lower():
+            spacer()
+            print("The distance is:",find_distance_time(time))
+            print("Work:")
+            print("0.5 * {time_print_ref}^2".format(time_print_ref=time))
         if "y" in continue_question.lower():
             spacer()
             continue
