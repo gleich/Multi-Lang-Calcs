c0019255d6846ffed729c01ed5805fb13ceb1876
Matt-Gleich <matthewgleich@gmail.com>
2019-05-01 21:00:24 -0400

    Creeate GCF.py

---
+++ Calculators/Math/Algebra_One/GCF.py
@@ -0,0 +1,37 @@
+def seperator():
+    """
+    Will clear the output screen for the user:
+    """
+    for i in range(50):
+        print("\n")
+
+
+def ask_question(question,cast_type):
+    """
+    Will ask the user a question and cast the answer.
+    :param question: the question that will be asked to the user
+    :param cast_type: what the question will be casted as
+    """
+    if "int" in cast_type:
+        users_input = int(input(question))
+        return users_input
+    elif "float" in cast_type:
+        users_input = float(input(question))
+        return users_input
+    elif "bool" in cast_type:
+        users_input = bool(input(question))
+        return users_input
+    elif "list" in cast_type:
+        users_input = list(input(question))
+        return users_input
+    else:
+        users_input = input(question)
+        return users_input
+
+
+
+
+
+while True:
+
+    ask_question = 
